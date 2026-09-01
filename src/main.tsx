3d21aac36aa27611099b10c4d9e5f506d03876f6
leecmn20-Em <leecmn20@gmail.com>
2026-09-02 01:09:29 +0900

    add prettier

--- src/main.tsx
+++ src/main.tsx
@@ -1,45 +1,53 @@
-import { StrictMode, type ComponentType, type ReactNode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import SiteLayout from './components/SiteLayout'
+import { StrictMode, type ComponentType, type ReactNode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import SiteLayout from "./components/SiteLayout";
 
 const pageModules = import.meta.glob<{ default: ComponentType }>(
-  './pages/**/page.tsx',
-)
+  "./pages/**/page.tsx",
+);
 
 function getPageModulePath(pathname: string) {
   const routePath = pathname
-    .replace(/\/index\.html$/, '/')
-    .replace(/^\/+|\/+$/g, '')
+    .replace(/\/index\.html$/, "/")
+    .replace(/^\/+|\/+$/g, "");
 
-  return routePath
-    ? `./pages/${routePath}/page.tsx`
-    : './pages/page.tsx'
+  return routePath ? `./pages/${routePath}/page.tsx` : "./pages/page.tsx";
 }
 
-const rootElement = document.getElementById('root')
+const rootElement = document.getElementById("root");
 
 if (!rootElement) {
-  throw new Error('#root 요소를 찾을 수 없습니다.')
+  throw new Error("#root 요소를 찾을 수 없습니다.");
 }
 
-const root = createRoot(rootElement)
+const root = createRoot(rootElement);
 
 function render(content: ReactNode) {
   root.render(
     <StrictMode>
       <SiteLayout>{content}</SiteLayout>
     </StrictMode>,
-  )
+  );
 }
 
-const pageModulePath = getPageModulePath(window.location.pathname)
-const loadPage = pageModules[pageModulePath]
+const pageModulePath = getPageModulePath(window.location.pathname);
+const loadPage = pageModules[pageModulePath];
 
 if (loadPage) {
   loadPage()
     .then(({ default: Page }) => render(<Page />))
-    .catch(() => render(<main><h1>페이지를 불러오지 못했습니다.</h1></main>))
+    .catch(() =>
+      render(
+        <main>
+          <h1>페이지를 불러오지 못했습니다.</h1>
+        </main>,
+      ),
+    );
 } else {
-  render(<main><h1>페이지를 찾을 수 없습니다.</h1></main>)
+  render(
+    <main>
+      <h1>페이지를 찾을 수 없습니다.</h1>
+    </main>,
+  );
 }
--- src/pages/about/page.tsx
+++ src/pages/about/page.tsx
@@ -4,7 +4,7 @@ function AboutPage() {
       <h1>E.m&apos;s blog</h1>
       <p>About 페이지가 준비되었습니다.</p>
     </main>
-  )
+  );
 }
 
-export default AboutPage
+export default AboutPage;
